a446e4dbd8f687b6e5a682ed1cd557469be0ae0d
alex-jones <abjhoops@gmail.com>
2023-01-31 21:28:49 -0700

    display list of projects in projects components

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,16 +2,14 @@ const projects = [
   {
     title: "connectFour",
     description: "Childhood strategy game rendered in a browser using HTML, CSS and Javascrypt",
-    image: "./connect-four.png",
-    // image: <img src="./connect-four.png" alt="Connect Four" />,
+    image: <img src="./connect-four.png" alt="Connect Four" />,
     repositoryLink: "https://github.com/alexanderjones1/connect-four.git",
     deploymentLink: "https://alexanderjones1connect4.netlify.app/",
   },
   {
     title: "rentMyCar",
     description: "A carsharing app where owners can list there cars for rent and users can shop cars they would like to rent for trips.",
-    image: "./rentMyCar.png",
-    // image: <img src="./rentMyCar.png" alt="Rent My Car landing page" />,
+    image: <img src="./rentMyCar.png" alt="Rent My Car landing page" />,
     repositoryLink: "https://github.com/alexanderjones1/rent-my-car-app.git",
     deploymentLink: "https://flyio-rent-my-car-project.fly.dev/",
   }
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,6 +1,19 @@
+import projects from "../data/projects"
+
 function Projects() {
   return (
+    <>
       <h1>Projects</h1>
+      <section>
+        {projects.map((project, index) =>
+        <li key={index}>
+          {project.title}
+          {project.image}
+          <button>Check if out</button>
+        </li>
+        )}
+      </section>
+    </>
   )
 }
 
