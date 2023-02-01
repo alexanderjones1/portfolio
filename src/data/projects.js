863a54b13ff181a52da432197c205608fb8fda85
alex-jones <abjhoops@gmail.com>
2023-01-31 21:17:39 -0700

    store project data in projects.js

---
+++ src/data/projects.js
@@ -0,0 +1,20 @@
+const projects = [
+  {
+    title: "connectFour",
+    description: "Childhood strategy game rendered in a browser using HTML, CSS and Javascrypt",
+    image: "./connect-four.png",
+    // image: <img src="./connect-four.png" alt="Connect Four" />,
+    repositoryLink: "https://github.com/alexanderjones1/connect-four.git",
+    deploymentLink: "https://alexanderjones1connect4.netlify.app/",
+  },
+  {
+    title: "rentMyCar",
+    description: "A carsharing app where owners can list there cars for rent and users can shop cars they would like to rent for trips.",
+    image: "./rentMyCar.png",
+    // image: <img src="./rentMyCar.png" alt="Rent My Car landing page" />,
+    repositoryLink: "https://github.com/alexanderjones1/rent-my-car-app.git",
+    deploymentLink: "https://flyio-rent-my-car-project.fly.dev/",
+  }
+]
+
+export default projects
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,11 @@
 function Contact() {
   return (
+    <>
       <h1>Contact</h1>
+      <h3>Email:</h3>
+      <h3>Github:</h3>
+      <h3>Linkedin:</h3>
+    </>
   )
 }
 
