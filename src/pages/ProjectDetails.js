61bd158d478c92c636b3a9f5f95ac1ddc3406c7d
alex-jones <abjhoops@gmail.com>
2023-02-01 21:29:44 -0700

    get dynamic project data

--- src/pages/ProjectDetails.js
+++ src/pages/ProjectDetails.js
@@ -1,10 +1,13 @@
-import Projects from "../data/Projects";
+import { useParams } from "react-router-dom";
+import findProject from "../utilities/findProject";
 
 function ProjectDetails() {
+  const {ProjectDetails} = useParams()
+  const project = findProject(ProjectDetails)
   return (
     <>
-      <h1>{Projects.title}</h1>
-      <p>{Projects.description}</p>
+      <h1>{project.title}</h1>
+      <p>{project.description}</p>
       {/* <img src="" /> */}
       <button>GitHub</button>
       <button>Deployed Site</button>
