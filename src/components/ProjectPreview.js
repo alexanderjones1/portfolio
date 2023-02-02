1d97219aa835c850c2f65b1714e95b9ba3ee0d23
alex-jones <abjhoops@gmail.com>
2023-02-01 20:22:52 -0700

    display data in ProjectPreview as card

--- src/components/ProjectPreview.js
+++ src/components/ProjectPreview.js
@@ -2,7 +2,21 @@ function ProjectPreview(props) {
   console.log("THESE ARE PROPS", props);
   return (
     <>
-      <h1>Preview</h1>
+      <div>
+    <img 
+        src={props.project.image} 
+        alt={props.project.title}
+    />
+
+    <section>
+        <h3>
+            {props.project.title}
+        </h3>
+				<button>
+            Learn more
+        </button>
+    </section>
+</div>
     </>
   )
 
--- src/data/projects.js
+++ src/data/projects.js
@@ -2,14 +2,14 @@ const Projects = [
   {
     title: "connectFour",
     description: "Childhood strategy game rendered in a browser using HTML, CSS and Javascrypt",
-    image: <img src="./connect-four.png" alt="Connect Four" />,
+    image: "./connect-four.png",
     repositoryLink: "https://github.com/alexanderjones1/connect-four.git",
     deploymentLink: "https://alexanderjones1connect4.netlify.app/",
   },
   {
     title: "rentMyCar",
     description: "A carsharing app where owners can list there cars for rent and users can shop cars they would like to rent for trips.",
-    image: <img src="./rentMyCar.png" alt="Rent My Car landing page" />,
+    image: "./rentMyCar.png",
     repositoryLink: "https://github.com/alexanderjones1/rent-my-car-app.git",
     deploymentLink: "https://flyio-rent-my-car-project.fly.dev/",
   }
