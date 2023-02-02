9b3380c14abd53595371e4c1d9f9c391951a1a0c
alex-jones <abjhoops@gmail.com>
2023-02-01 22:13:23 -0700

    clean up code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ function Home() {
     <>
       <h1>Alex Jones</h1>
       <h2>Software Engineer</h2>
-      <img src="./profile-pic.jpeg" alt="Profile Picture" />
+      <img src="./profile-pic.jpeg" alt="Profile" />
     </>
   )
 }
