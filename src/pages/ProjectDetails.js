3b6523e84d81cdd6e21c97ed1f52eb7bd0d2d250
alex-jones <abjhoops@gmail.com>
2023-02-01 20:42:06 -0700

    create ProjectDetail component and add a route to it in the App

---
+++ src/pages/ProjectDetails.js
@@ -0,0 +1,15 @@
+import Projects from "../data/projects";
+
+function ProjectDetails() {
+  return (
+    <>
+      <h1>Project Title</h1>
+      <p>Description</p>
+      {/* <img src="" /> */}
+      <button>GitHub</button>
+      <button>Deployed Site</button>
+    </>
+  )
+}
+
+export default ProjectDetails
