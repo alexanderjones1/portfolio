c6f5aa4ad0c7d061d96a0d2a5801e1dff05ac3fb
alex-jones <abjhoops@gmail.com>
2023-02-01 20:16:12 -0700

    create project preview and pass it project title as prop

---
+++ src/components/ProjectPreview.js
@@ -0,0 +1,11 @@
+function ProjectPreview(props) {
+  console.log("THESE ARE PROPS", props);
+  return (
+    <>
+      <h1>Preview</h1>
+    </>
+  )
+
+}
+
+export default ProjectPreview
--- src/components/ProjectsList.js
+++ src/components/ProjectsList.js
@@ -1,14 +1,13 @@
+import ProjectPreview from "./ProjectPreview";
+
 function ProjectsList(props) {
-  console.log("this is props>>>>>", props);
   return (
     <>
       <h1>Projects</h1>
       <section>
         {props.projects.map((project) =>
         <li key={project.title}>
-          {project.title}
-          {project.image}
-          <button>Check if out</button>
+          <ProjectPreview project={project} />
         </li>
         )}
       </section>
